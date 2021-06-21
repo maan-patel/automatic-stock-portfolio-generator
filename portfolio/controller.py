4d6b1de6278dff04e9f5fc2792f6da6720d921e1
Maan Patel <79115388+maan-patel@users.noreply.github.com>
2021-06-21 14:39:21 -0400

    Updated Portfolio

--- portfolio/controller.py
+++ portfolio/controller.py
@@ -32,7 +32,7 @@ def recommended_tickers(stock_ticker):
     querystring_recommender = {"symbol": stock_ticker}
 
     headers_recommender = {
-        'x-rapidapi-key': "3b5eefa820msha0498f1c4c42783p194c3cjsnb6c5e4bc24a3",
+        'x-rapidapi-key': "1ef84c9bc0msh0ec5f4323cd2733p19e01fjsn40a6d4d91c6e",
         'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
     }
 
@@ -61,7 +61,7 @@ def recommended_stock_weight(stock_ticker):
     querystring_recommender = {"symbol": stock_ticker}
 
     headers_recommender = {
-        'x-rapidapi-key': "3b5eefa820msha0498f1c4c42783p194c3cjsnb6c5e4bc24a3",
+        'x-rapidapi-key': "1ef84c9bc0msh0ec5f4323cd2733p19e01fjsn40a6d4d91c6e",
         'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
     }
 
@@ -87,7 +87,7 @@ def is_valid_ticker(stock_ticker):
     querystring = {"region": "US", "symbols": stock_ticker}
 
     headers = {
-        'x-rapidapi-key': "3b5eefa820msha0498f1c4c42783p194c3cjsnb6c5e4bc24a3",
+        'x-rapidapi-key': "1ef84c9bc0msh0ec5f4323cd2733p19e01fjsn40a6d4d91c6e",
         'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
     }
 
@@ -211,7 +211,7 @@ def sentiment_for_news():
     payload = "Pass in the value of uuids field returned right in this endpoint to load the next page, or leave empty to load first page"
     headers = {
         'content-type': "text/plain",
-        'x-rapidapi-key': "3b5eefa820msha0498f1c4c42783p194c3cjsnb6c5e4bc24a3",
+        'x-rapidapi-key': "1ef84c9bc0msh0ec5f4323cd2733p19e01fjsn40a6d4d91c6e",
         'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
         }
 
